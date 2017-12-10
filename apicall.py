13f140b9fe46ba7cfad947125456fa5a67500ab9
Shuaib Munshi <shuaib@hyperbolix.local>
2017-12-10 00:23:54 -0500

    renamed api call file, return from the api call file is now a dict, added dict parsing to main html return function.

--- apicall.py
+++ apicall.py
@@ -3,26 +3,34 @@ import json
 import os
 import settings
 
-def getstoptimes():
+def getstoptimes(stop_number):
     api_parameters = [
       ('appID', settings.OCTRASPOAPPID), #appID and apiKey are in hidden file settings.py
       ('apiKey', settings.OCTRANSPOAPIKEY),
       ('routeNo', '86'),
-      ('stopNo', '6032'),
+      ('stopNo', stop_number),
       ('format', 'json'),
       ]
-    requeststop = requests.post('https://api.octranspo1.com/v1.2/GetNextTripsForStop', data=api_parameters)
+    api_response = requests.post('https://api.octranspo1.com/v1.2/GetNextTripsForStop', data=api_parameters)
     #print(requeststop.text) #this is for debugging, it prints the json as a string into the shell
 
     #use example json below if you're staying up too late and there are no more trips to fetch from OCtranspo
     example_json = '{"GetNextTripsForStopResult":{"StopNo":"6032","StopLabel":"FISHER DYNES","Error":"","Route":{"RouteDirection":{"RouteNo":86,"RouteLabel":"Elmvale","Direction":"Eastbound","Error":"","RequestProcessingTime":"20171208115954","Trips":{"Trip":[{"TripDestination":"Hurdman","TripStartTime":"11:50","AdjustedScheduleTime":"9","AdjustmentAge":"0.85","LastTripOfSchedule":false,"BusType":"4E DEH","Latitude":"45.342767","Longitude":"-75.728672","GPSSpeed":"0.5"},{"TripDestination":"Elmvale","TripStartTime":"12:02","AdjustedScheduleTime":"19","AdjustmentAge":"0.72","LastTripOfSchedule":false,"BusType":"4LA - DEH","Latitude":"45.348399","Longitude":"-75.762452","GPSSpeed":"0.5"},{"TripDestination":"Hurdman","TripStartTime":"12:18","AdjustedScheduleTime":"34","AdjustmentAge":"-1","LastTripOfSchedule":false,"BusType":"4E - DEH","Latitude":"","Longitude":"","GPSSpeed":""}]}}}}}'
 
-    #parsed_json = json.loads(requeststop.text) #loads is load string r.text is the text format
+    #parsed_json = json.loads(api_response.text) #loads is load string r.text is the text format
     parsed_json = json.loads(example_json) #Uses the example json object
 
-    stop_label = parsed_json['GetNextTripsForStopResult']['StopLabel']
-    trip_start_time_0 = parsed_json['GetNextTripsForStopResult']['Route']['RouteDirection']['Trips']['Trip'][0]['TripStartTime']
-    return trip_start_time_0
+    parsed_dict = {}
+    parsed_dict["stop_label"] = parsed_json['GetNextTripsForStopResult']['StopLabel']
+    parsed_dict["route_number"] = parsed_json['GetNextTripsForStopResult']['Route']['RouteDirection']['RouteNo']
+    parsed_dict["trip_destination_0"] = parsed_json['GetNextTripsForStopResult']['Route']['RouteDirection']['Trips']['Trip'][0]['TripDestination']
+    parsed_dict["scheduled_stop_time_0"] = parsed_json['GetNextTripsForStopResult']['Route']['RouteDirection']['Trips']['Trip'][0]['TripStartTime']
+    parsed_dict["schedule_adjustment_time_0"] = parsed_json['GetNextTripsForStopResult']['Route']['RouteDirection']['Trips']['Trip'][0]['AdjustedScheduleTime']
+    parsed_dict["trip_destination_1"] = parsed_json['GetNextTripsForStopResult']['Route']['RouteDirection']['Trips']['Trip'][1]['TripDestination']
+    parsed_dict["scheduled_stop_time_1"] = parsed_json['GetNextTripsForStopResult']['Route']['RouteDirection']['Trips']['Trip'][1]['TripStartTime']
+    parsed_dict["schedule_adjustment_time_1"] = parsed_json['GetNextTripsForStopResult']['Route']['RouteDirection']['Trips']['Trip'][1]['AdjustedScheduleTime']
+
+    return parsed_dict
     #print(parsed_json['StopLabel'])
 
 
--- app.py
+++ app.py
@@ -4,22 +4,23 @@ import socket
 import requests
 import json
 import settings
-from stop_6032 import getstoptimes
+from apicall import getstoptimes
 
 
 app = Flask(__name__)
 
 @app.route("/")
 def main():
-    #apiparameters = {'appID': settings.OCTRASPOAPPID,'apiKey': settings.OCTRANSPOAPIKEY,'routeNo': '86','stopNo': '6032','format': 'json'}
-    #r = requests.post('https://api.octranspo1.com/v1.2/GetNextTripsForStop', data = apiparameters)
-    #parsed_json = json.loads(r.text) #loads is load string r.text is the text format
-    #jsonstoptime = parsed_json['GetNextTripsForStopResult']['StopNo']
-    stoptimes = getstoptimes()
-    html = "<h3>Stop 6032, Northbound:</h3>" \
-           "<b>Stoptime:</b> {functionstoptime}<br/>" \
-           "<b>Hostname:</b> {hostname}<br/>"
-    return html.format(hostname=socket.gethostname(), functionstoptime=stoptimes)
+    stoptimes = getstoptimes(stop_number=6032) #Parameter is the stop number
+    html = "<h3>Bus Stop 6032: {stop_label}</h3>" \
+           "<b>Bus Number:</b> {route_number}</br>" \
+           "<b>Trip Destination:</b> {trip_destination_0}</br>" \
+           "<b>Scheduled Stop:</b> {scheduled_stop_time_0}<br/>"
+    return html.format(stop_label=stoptimes['stop_label'],
+                       route_number=stoptimes['route_number'],
+                       trip_destination_0=stoptimes['trip_destination_0'],
+                       scheduled_stop_time_0=stoptimes['scheduled_stop_time_0']
+                       )
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80)
