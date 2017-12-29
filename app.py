9c66de79ab7dbd8db68e30d6cc6cbc7ce0b36eb5
Shuaib Munshi <shuaib@hyperbolix.local>
2017-12-29 04:00:47 -0500

    Added style.css to make the output pretty. Removed the silly bold tags in the html template and make it a paragraph instead

--- app.py
+++ app.py
@@ -1,18 +1,17 @@
 from flask import Flask
 from flask import render_template
-import os
-import socket
-import requests
 import json
 import settings
 from apicall import getstoptimes
 from gpstime import getgpstime
+from datetime import datetime
 
 
 app = Flask(__name__)
 
 @app.route("/")
 def main():
+    current_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
     stoptimes = getstoptimes(stop_number=6032) #Parameter is the stop number
     #Fetches the GPS time using the scheduled time and the time delta from the GPS position of the bus
     real_gps_time = getgpstime(stop_time=stoptimes['scheduled_stop_time_0'],
@@ -24,7 +23,8 @@ def main():
                         trip_destination_0=stoptimes['trip_destination_0'],
                         scheduled_stop_time_0=stoptimes['scheduled_stop_time_0'],
                         schedule_adjustment_time_0=stoptimes['schedule_adjustment_time_0'],
-                        real_gps_time=real_gps_time
+                        real_gps_time=real_gps_time,
+                        current_time=current_time
                         )
 
 if __name__ == "__main__":
