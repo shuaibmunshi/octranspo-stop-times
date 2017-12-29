cf3122096861fe019212595e8e2e0b188e39f7db
Shuaib Munshi <munshi.shuaib@gmail.com>
2017-12-29 06:10:39 -0500

    Added stop info for bus stop 6031, AKA the other side of the street. Cleaned up the CSS a little bit.

--- app.py
+++ app.py
@@ -11,31 +11,39 @@ app = Flask(__name__)
 
 @app.route("/")
 def main():
-    current_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-    stoptimes = getstoptimes(stop_number=6032) #Parameter is the stop number
+    current_time = datetime.now().strftime('%H:%M:%S')
+    stoptimes6032 = getstoptimes(stop_number=6032) #Parameter is the stop number
+    stoptimes6031 = getstoptimes(stop_number=6031) #Parameter is the stop number
     #Fetches the GPS time using the scheduled time and the time delta from the GPS position of the bus
-    real_gps_time_0 = getgpstime(
-    stop_time=stoptimes['scheduled_stop_time_0'],
-    adjustment_time=stoptimes['schedule_adjustment_time_0']
-    )
-    real_gps_time_1 = getgpstime(
-    stop_time=stoptimes['scheduled_stop_time_1'],
-    adjustment_time=stoptimes['schedule_adjustment_time_1']
-    )
+    real_gps_time_6032_0 = getgpstime(stop_time=stoptimes6032['scheduled_stop_time_0'],adjustment_time=stoptimes6032['schedule_adjustment_time_0'])
+    real_gps_time_6032_1 = getgpstime(stop_time=stoptimes6032['scheduled_stop_time_1'],adjustment_time=stoptimes6032['schedule_adjustment_time_1'])
     #render_template is a Flask function that returns renders what is in app/templates
+
+    real_gps_time_6031_0 = getgpstime(stop_time=stoptimes6031['scheduled_stop_time_0'],adjustment_time=stoptimes6032['schedule_adjustment_time_0'])
+    real_gps_time_6031_1 = getgpstime(stop_time=stoptimes6031['scheduled_stop_time_1'],adjustment_time=stoptimes6032['schedule_adjustment_time_1'])
+
     return render_template(
     'index.html',
-    stop_label=stoptimes['stop_label'],
-    route_number=stoptimes['route_number'],
-    trip_destination_0=stoptimes['trip_destination_0'],
-    scheduled_stop_time_0=stoptimes['scheduled_stop_time_0'],
-    schedule_adjustment_time_0=stoptimes['schedule_adjustment_time_0'],
-    real_gps_time_0=real_gps_time_0,
-    trip_destination_1=stoptimes['trip_destination_1'],
-    scheduled_stop_time_1=stoptimes['scheduled_stop_time_1'],
-    schedule_adjustment_time_1=stoptimes['schedule_adjustment_time_1'],
-    real_gps_time_1=real_gps_time_1,
-    current_time=current_time
+    stop_label_6032=stoptimes6032['stop_label'],
+    route_number=stoptimes6032['route_number'],
+    trip_destination_6032_0=stoptimes6032['trip_destination_0'],
+    scheduled_stop_time_6032_0=stoptimes6032['scheduled_stop_time_0'],
+    schedule_adjustment_time_6032_0=stoptimes6032['schedule_adjustment_time_0'],
+    real_gps_time_6032_0=real_gps_time_6032_0,
+    trip_destination_6032_1=stoptimes6032['trip_destination_1'],
+    scheduled_stop_time_6032_1=stoptimes6032['scheduled_stop_time_1'],
+    schedule_adjustment_time_6032_1=stoptimes6032['schedule_adjustment_time_1'],
+    real_gps_time_6032_1=real_gps_time_6032_1,
+    current_time=current_time,
+    stop_label_6031=stoptimes6031['stop_label'],
+    trip_destination_6031_0=stoptimes6031['trip_destination_0'],
+    scheduled_stop_time_6031_0=stoptimes6031['scheduled_stop_time_0'],
+    schedule_adjustment_time__0=stoptimes6031['schedule_adjustment_time_0'],
+    real_gps_time_6031_0=real_gps_time_6031_0,
+    trip_destination_6031_1=stoptimes6031['trip_destination_1'],
+    scheduled_stop_time_6031_1=stoptimes6031['scheduled_stop_time_1'],
+    schedule_adjustment_time_6031_1=stoptimes6031['schedule_adjustment_time_1'],
+    real_gps_time_6031_1=real_gps_time_6031_1
     )
 
 if __name__ == "__main__":
