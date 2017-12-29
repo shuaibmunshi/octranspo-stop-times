63379cd2ccce78b9001ccb059e88a3bbb3f18145
Shuaib Munshi <munshi.shuaib@gmail.com>
2017-12-29 05:23:18 -0500

    cleaned up index.css. Added the 2nd stop time and made the HTML display both stop times in two collumns

--- app.py
+++ app.py
@@ -14,18 +14,29 @@ def main():
     current_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
     stoptimes = getstoptimes(stop_number=6032) #Parameter is the stop number
     #Fetches the GPS time using the scheduled time and the time delta from the GPS position of the bus
-    real_gps_time = getgpstime(stop_time=stoptimes['scheduled_stop_time_0'],
-                    adjustment_time=stoptimes['schedule_adjustment_time_0'])
+    real_gps_time_0 = getgpstime(
+    stop_time=stoptimes['scheduled_stop_time_0'],
+    adjustment_time=stoptimes['schedule_adjustment_time_0']
+    )
+    real_gps_time_1 = getgpstime(
+    stop_time=stoptimes['scheduled_stop_time_1'],
+    adjustment_time=stoptimes['schedule_adjustment_time_1']
+    )
     #render_template is a Flask function that returns renders what is in app/templates
-    return render_template('index.html',
-                        stop_label=stoptimes['stop_label'],
-                        route_number=stoptimes['route_number'],
-                        trip_destination_0=stoptimes['trip_destination_0'],
-                        scheduled_stop_time_0=stoptimes['scheduled_stop_time_0'],
-                        schedule_adjustment_time_0=stoptimes['schedule_adjustment_time_0'],
-                        real_gps_time=real_gps_time,
-                        current_time=current_time
-                        )
+    return render_template(
+    'index.html',
+    stop_label=stoptimes['stop_label'],
+    route_number=stoptimes['route_number'],
+    trip_destination_0=stoptimes['trip_destination_0'],
+    scheduled_stop_time_0=stoptimes['scheduled_stop_time_0'],
+    schedule_adjustment_time_0=stoptimes['schedule_adjustment_time_0'],
+    real_gps_time_0=real_gps_time_0,
+    trip_destination_1=stoptimes['trip_destination_1'],
+    scheduled_stop_time_1=stoptimes['scheduled_stop_time_1'],
+    schedule_adjustment_time_1=stoptimes['schedule_adjustment_time_1'],
+    real_gps_time_1=real_gps_time_1,
+    current_time=current_time
+    )
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80)
